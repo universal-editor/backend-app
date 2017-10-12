8150254788185dd87941432538594b8df5b650df
Nik Samokhvalov <nik@samokhvalov.info>
2017-10-12 17:08:43 +0300

    Disable cache component

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,5 @@
 <?php
+
 return [
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor'
 ];
