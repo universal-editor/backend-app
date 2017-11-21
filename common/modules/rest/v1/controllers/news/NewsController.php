07b83d5501b3fc64ee7496d9e920f890d25c01c7
Nik Samokhvalov <nik@samokhvalov.info>
2017-11-21 15:09:50 +0300

    423 error for DELETE requests

--- common/modules/rest/v1/controllers/news/NewsController.php
+++ common/modules/rest/v1/controllers/news/NewsController.php
@@ -7,6 +7,7 @@ use common\modules\rest\v1\controllers\news\actions\UnlockAction;
 use common\modules\rest\v1\models\news\News;
 use yii\rest\ActiveController;
 use yii\rest\Serializer;
+use yii\web\HttpException;
 
 /**
  * News controller.
@@ -23,6 +24,15 @@ class NewsController extends ActiveController
     public function actions()
     {
         $actions = parent::actions();
+        
+        $actions['delete']['checkAccess'] = function ($action, $model) {
+            if (in_array($model->id, [3, 4])) {
+                throw new HttpException(
+                    LockAction::HTTP_LOCKED,
+                    sprintf('Entity "%d" is already locked.', $model->id)
+                );
+            }
+        };
 
         $actions['lock'] = [
             'class' => LockAction::class,
