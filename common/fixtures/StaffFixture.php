50969768454273f867dab8c7c144e6a3aaefc044
Nik Samokhvalov <nik@samokhvalov.info>
2017-12-27 11:27:37 +0300

    New fixtures

---
+++ common/fixtures/StaffFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace common\fixtures;
+
+use common\modules\rest\v1\models\organization\Staff;
+use yii\test\ActiveFixture;
+
+class StaffFixture extends ActiveFixture
+{
+    public $modelClass = Staff::class;
+}
--- common/fixtures/data/tag.php
+++ common/fixtures/data/tag.php
@@ -3,19 +3,19 @@
 return [
     [
         'id' => 1,
-        'name' => 'OM',
+        'name' => 'Sport',
         'created_at' => 814125491,
         'updated_at' => 941802416,
     ],
     [
         'id' => 2,
-        'name' => 'PT',
+        'name' => 'Culture',
         'created_at' => 1128418776,
         'updated_at' => 1397794112,
     ],
     [
         'id' => 3,
-        'name' => 'IQ',
+        'name' => 'Technology',
         'created_at'  => 1513242840,
         'updated_at'  => 1513249020,
     ],
